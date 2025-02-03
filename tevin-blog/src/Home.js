3a8a08268ed192391a310f63e8cb0341b8596d35
Tevin Achong <tevinachong@macbookair.lan>
2025-02-03 17:56:06 -0400

    handled fetching data from db.json

--- tevin-blog/src/Home.js
+++ tevin-blog/src/Home.js
@@ -2,38 +2,27 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('Killua');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     // This function runs every time there is a re-render (the data changes)
     useEffect(() => {
-        console.log('Name has been changed!!!!');
-        console.log(`It is now ${name}`);
+        fetch('http://localhost:8000/blogs').then(
+            res => {
+                return res.json();
+            }
+        ).then(data => {
+            console.log(data);
+            setBlogs(data);
+        });
         // We can access the state
-    }, [name]);  
+    }, []);  
     // passing a dependency array to specify when useEffect should run (not after every render)
     // an empty array makes sure the function is only run once, on the first render
-    // Since we pass [name] useEffect will only run when there is a change to the state of `name`
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} homePageTitle="All Blogs!" handleDelete={handleDelete}/>
-            
-            {/* Component being reused */}
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} homePageTitle="Mario's Blogs!"/>
-
-            <button onClick={() => setName(name === 'Killua' ? 'Gojo' : 'Killua')}>change name</button>
-            <p>{ name }</p>
+            {/* If blogs is null, it does not proceed with creating the component */}
+            {blogs && <BlogList blogs={blogs} homePageTitle="All Blogs!"/>} 
         </div>
      );
 }
