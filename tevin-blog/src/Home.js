300db7c4902d5367d7f24b8c4b30afdaa6a5a89c
Tevin Achong <tevinachong@macbookair.lan>
2025-02-03 18:24:33 -0400

    toggle error and pending messages based on fetch status

--- tevin-blog/src/Home.js
+++ tevin-blog/src/Home.js
@@ -4,18 +4,26 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // This function runs every time there is a re-render (the data changes)
     useEffect(() => {
         setTimeout(() => { 
             fetch('http://localhost:8000/blogs').then(
                 res => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource.');
+                    }
                     return res.json();
                 }
             ).then(data => {
                 setBlogs(data);
                 setIsPending(false);
-            });
+                setError(null);
+            }).catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            })
         }, 1000)
         // We can access the state
     }, []);  
@@ -24,6 +32,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Fetching data...</div> }
 
             {/* If blogs is null, it does not proceed with creating the component */}
